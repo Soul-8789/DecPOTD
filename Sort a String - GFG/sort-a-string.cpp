84460dec10473706c5a655d67fa605a0c0e7122a
Soul-8789 <102282026+Soul-8789@users.noreply.github.com>
2023-04-13 16:46:52 +0530

    Added solution - LeetHub

---
+++ Sort a String - GFG/sort-a-string.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+string sort(string s);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        cout << sort(s) << endl;
+    }
+return 0;
+}
+
+
+
+
+// } Driver Code Ends
+
+
+string sort(string s){
+    //complete the function here
+    map<char,int> mp;
+    for(auto x:s)
+    {
+        mp[x]++;
+    }
+    string res="";
+    for(auto x:mp)
+    {
+        int t=x.second;
+        for(int i=0;i<t;i++)
+        {
+            res+=x.first;
+        }
+    }
+    return res;
+}
