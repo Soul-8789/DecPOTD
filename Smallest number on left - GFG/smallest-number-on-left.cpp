412b0ecec06fe2ae9745c8e9993bd74ddb5d0564
Baijnath Mahto <102282026+Soul-8789@users.noreply.github.com>
2023-08-20 16:28:00 +0530

    Added solution - LeetHub

--- Smallest number on left - GFG/smallest-number-on-left.cpp
+++ Smallest number on left - GFG/smallest-number-on-left.cpp
@@ -12,26 +12,34 @@ public:
     vector<int> leftSmaller(int n, int arr[]){
         // code here
         // Your code here
-        vector<int> ans(n);
+   vector<int> ans;
         stack<int> st;
-       
-       for(int i=0;i<n;i++)
-       {
-           ans[i]=-1;
-           while(!st.empty())
-           {
-               int top=st.top();
-               if(top< arr[i]){
-                   ans[i]=top;
-                   break;
-               }
-               else{
-                   st.pop();
-               }
-           }
-           st.push(arr[i]);
-       }
-       return ans;
+        for(int i=0;i<n;i++)
+        {
+            if(st.size()==0)
+            {
+                ans.push_back(-1);
+            }
+            else if(st.size()>0 && st.top()<arr[i]){
+                ans.push_back(st.top());
+            }
+            else if(st.size()> 0 && st.top()>=arr[i]){
+                while(st.size()>0 && st.top()>=arr[i]){
+                    st.pop();
+                }
+                if(st.size()==0){
+                  ans.push_back(-1);
+                }
+                else{
+              ans.push_back(st.top());
+                }
+            }
+            st.push(arr[i]);
+            
+        }
+
+        
+        return ans;
     
     }
 };
