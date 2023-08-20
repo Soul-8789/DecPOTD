8fbb822e2c0509d07a6ec4e27b157ebb1ace84a6
Baijnath Mahto <102282026+Soul-8789@users.noreply.github.com>
2023-08-20 16:18:03 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.cpp
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -12,32 +12,24 @@ class Solution
         // Your code here
         vector<long long> ans;
         stack<long long> st;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(st.size()==0)
-            {
-                ans.push_back(-1);
-            }
-            else if(st.size()>0 && st.top()> arr[i]){
-                ans.push_back(st.top());
-            }
-            else if(st.size()> 0 && st.top()<=arr[i]){
-                while(st.size()>0 && st.top()<=arr[i]){
-                    st.pop();
-                }
-                if(st.size()==0){
-                  ans.push_back(-1);
-                }
-                else{
-              ans.push_back(st.top());
-                }
-            }
-            st.push(arr[i]);
-            
-        }
-        reverse(ans.begin(),ans.end());
-        
-        return ans;
+       
+       for(int i=n-1;i>=0;i--)
+       {
+           ans[i]=-1;
+           while(!st.empty())
+           {
+               int top=st.top();
+               if(top> arr[i]){
+                   ans.push_back(top);
+                   break;
+               }
+               else{
+                   st.pop();
+               }
+           }
+           st.push(arr[i]);
+       }
+       return ans;
     }
 };
 
