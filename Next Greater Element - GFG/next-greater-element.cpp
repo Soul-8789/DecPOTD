9bb1ff7201c353252415694f2656f6aa83fc0aea
Baijnath Mahto <102282026+Soul-8789@users.noreply.github.com>
2023-08-20 15:56:59 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.cpp
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -4,31 +4,40 @@ using namespace std;
 
 
 // } Driver Code Ends
-// User Function Template for C++ solution
-
-class Solution {
-  public:
-    vector<long long> nextLargerElement(vector<long long> &arr, int n){
-   stack<long long> st;
-
-        vector<long long> ans(n);
-
-        for(int i=n-1;i>=0;i--){
-
-            while(!st.empty() && st.top()<=arr[i])
-
-            st.pop();
-
-            if(st.empty()) ans[i] = -1;
-
-            else ans[i] = st.top();
-
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        vector<long long> ans;
+        stack<long long> st;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(st.size()==0)
+            {
+                ans.push_back(-1);
+            }
+            else if(st.size()>0 && st.top()> arr[i]){
+                ans.push_back(st.top());
+            }
+            else if(st.size()> 0 && st.top()<=arr[i]){
+                while(st.size()>0 && st.top()<=arr[i]){
+                    st.pop();
+                }
+                if(st.size()==0){
+                  ans.push_back(-1);
+                }
+                else{
+              ans.push_back(st.top());
+                }
+            }
             st.push(arr[i]);
-
+            
         }
-
+        reverse(ans.begin(),ans.end());
+        
         return ans;
-
     }
 };
 
@@ -47,13 +56,11 @@ int main()
         for(int i=0;i<n;i++)
             cin>>arr[i];
         
-        Solution ob;
-        
-        vector <long long> res = ob.nextLargerElement(arr, n);
+        Solution obj;
+        vector <long long> res = obj.nextLargerElement(arr, n);
         for (long long i : res) cout << i << " ";
         cout<<endl;
     }
 	return 0;
 }
-
 // } Driver Code Ends
