d1c2d2b014c6927f9f037737c353f46d87018041
Baijnath Mahto <102282026+Soul-8789@users.noreply.github.com>
2023-08-25 23:36:44 +0530

    Added solution - LeetHub

--- Palindrome String - GFG/palindrome-string.cpp
+++ Palindrome String - GFG/palindrome-string.cpp
@@ -13,17 +13,14 @@ public:
 	int isPalindrome(string S)
 	{
 	    // Your code goes here
-	    int n=S.size();
-	    int i=0,j=n-1;
-	    while(i<=j)
+	    int i=0,j=S.size()-1;
+	    while(i<j)
 	    {
-	        if(S[i]!=S[j]){
-	        return 0;       
+	        if(S[i++]!=S[j--]){
+	            return false;
 	        }
-	     i++;
-	     j--;
 	    }
-	    return 1;
+	    return true;
 	}
 
 };
